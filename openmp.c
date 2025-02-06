94c907da6a12d41ef4990e34f6f2c5e2992f5fa1
vishalgujare27 <71836388+vishalgujare27@users.noreply.github.com>
2025-02-07 00:00:33 +0530

    Update openmp.c

--- openmp.c
+++ openmp.c
@@ -2,15 +2,22 @@
 #include<stdlib.h>
 #include<math.h>
 #include<complex.h>
+#include<omp.h>
 #include<lapacke.h>
 #include<cblas.h>
-#include<omp.h> // OpenMP header
 
 #define PI 3.1415926535897932
 
+
 double potential(double x){
-    return 0.50*x*x;
+  
+  double pot;
+
+  pot = 0.50*x*x;  
+
+  return pot;
 }
+
 
 double complex **calloc_2d_array_complex(int rows, int cols) {
     double complex **array = (double complex**)calloc(rows, sizeof(double complex*));
@@ -19,7 +26,7 @@ double complex **calloc_2d_array_complex(int rows, int cols) {
         printf("Memory allocation failed for rows!\n");
         exit(1);
     }
-
+#pragma omp parallel for
     for (int i = 0; i < rows; i++) {
         array[i] = (double complex*)calloc(cols, sizeof(double complex));
         if (array[i] == NULL) {
@@ -31,16 +38,22 @@ double complex **calloc_2d_array_complex(int rows, int cols) {
     return array;
 }
 
+
 void free_2d_array_complex(double complex** array, int rows) {
-    for (int i = 0; i < rows; i++) {
+ #pragma omp parallel for
+     	for (int i = 0; i < rows; i++) {
         free(array[i]);
     }
     free(array);
 }
 
+
+
 void copy_matrix_complex_to_complex(int ncsf, double complex **source, double complex **dest) {
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < ncsf; i++) {
+  
+#pragma omp parallel for collapse(2)      
+
+	for (int i = 0; i < ncsf; i++) {
         for (int j = 0; j < ncsf; j++) {
             dest[i][j] = source[i][j];
         }
@@ -51,22 +64,38 @@ void copy_matrix_real_to_complex(int ncsf, double **source, double complex **des
     #pragma omp parallel for collapse(2)
     for (int i = 0; i < ncsf; i++) {
         for (int j = 0; j < ncsf; j++) {
+            dest[i][j] = source[i][j] + 0.0 * I;  // Direct assignment
+        }
+    }
+}
+/*void copy_matrix_real_to_complex(int ncsf, double **source, double complex **dest) {
+  
+      	for (int i = 0; i < ncsf; i++) {
+        for (int j = 0; j < ncsf; j++) {
             __real__ dest[i][j] = source[i][j];
             __imag__ dest[i][j] = 0.0;
         }
     }
-}
+} */
+
 
 int timeprop(int ncsf, double xmin, double xmax, double lmda, int flqchnl, int noptc, 
              int istate, double totime, int ntim, double omga, double epsln){
 
   double mass = 10000.0; 
+  
   double dtim = totime/ntim;
+
   double dx = (xmax-xmin)/(ncsf-1);
+	   
   int tchnl = 2*flqchnl+1;
+
   int nfloq = ncsf*tchnl;
+
   int inzr = flqchnl*ncsf;
+
   double tau = 2.0*PI/omga;
+
 
   printf("dx = %lf \n", dx);
   printf("length = %lf \n", (xmax-xmin));
@@ -77,96 +106,158 @@ int timeprop(int ncsf, double xmin, double xmax, double lmda, int flqchnl, int n
   printf("epsln = %lf \n", epsln);
   printf("tchnl = %d \n", tchnl);
 
+
   double **hmt = (double**)calloc(ncsf,sizeof(double*));
-  for (int i = 0; i < ncsf; i++) {
+  #pragma omp parallel for
+  for (int i=0; i<ncsf; i++){
     hmt[i] = (double*)calloc(ncsf,sizeof(double));
   }
 
-  // kinetic energy
-  #pragma omp parallel for collapse(2)
-  for (int k = 0; k < ncsf; k++) {
+
+  // kinetic energy 
+  
+  #pragma omp parallel for
+
+  for(int k=0; k<ncsf; k++){
     hmt[k][k] = (PI*PI)/(6.0*dx*dx);
-    for (int j = 0; j < k; j++) {
+
+    for(int j=0; j<k; j++){	    
       hmt[k][j] = (pow(-1.0,(k-j)))/((k-j)*(k-j)*dx*dx);
       hmt[j][k] = hmt[k][j];
     }
   }
 
-  // potential energy modification
+
+  FILE *file = fopen("./result/kinetic.txt","w");  
+    #pragma omp parallel for
+
+  for(int i=0; i<ncsf; i++){
+    for(int j=0; j<ncsf; j++){
+ #pragma omp critical
+    	    fprintf(file,"%e ",hmt[i][j]);
+    }	  
+     #pragma omp critical
+    fprintf(file,"\n");
+  }
+
+#pragma omp parallel for collapse(2)
+for (int i = 0; i < ncsf; i++) {
+    for (int j = 0; j < ncsf; j++) {
+        hmt[i][j] = hmt[i][j] / 10000.0;
+    }
+}
+
+
   double *zdipole, *cpm;  
   zdipole = (double *)calloc(ncsf,sizeof(double));
   cpm = (double *)calloc(ncsf,sizeof(double));
-
-  #pragma omp parallel for
-  for (int i = 0; i < ncsf; i++) {
+#pragma omp parallel for
+  for(int i=0; i<ncsf; i++){
     double x = xmin + i*dx;
     zdipole[i] = x;
+     
     hmt[i][i] = hmt[i][i] + potential(x);
-    if (abs(x) > (xmax-30.0)) {
+
+    if (abs(x) > (xmax-30.0)){
       cpm[i] = (abs(x)-(xmax-30.0))*(abs(x)-(xmax-30.0));
     }
   }
 
+
   double *h; 
   h = (double *)calloc(ncsf*ncsf,sizeof(double));
+      #pragma omp parallel for collapse(2)
 
-  #pragma omp parallel for collapse(2)
-  for (int i = 0; i < ncsf; i++) {
-    for (int j = 0; j < ncsf; j++) {
-      h[i*ncsf+j] = hmt[i][j];
-    }
+  for(int i=0; i<ncsf; i++){
+    for(int j=0; j<ncsf; j++){
+    	h[i*ncsf+j] = hmt[i][j];
+    }	  
   }
+
 
   double *eig = (double *)calloc(ncsf,sizeof(double));  
   int info;
+
   info = LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', ncsf, h, ncsf, eig);
+
+
   if (info != 0) {
       printf("Error in LAPACKE_dsyev: %d\n",info);
+//      free(h);
+//      free(W);
       return -1;
   }
-
-  // Copy results into hmt
+  
+  
   #pragma omp parallel for collapse(2)
-  for (int i = 0; i < ncsf; i++) {
-    for (int j = 0; j < ncsf; j++) {
+  for(int i=0; i<ncsf; i++){
+    for(int j=0; j<ncsf; j++){
       hmt[i][j] = h[i*ncsf+j];
     }
   }
 
-  double complex *cofi = (double complex*)calloc(nfloq, sizeof(double complex));
 
-  #pragma omp parallel for
-  for (int i = 0; i < ncsf; i++) {
-    cofi[inzr + i] = hmt[i][istate];
-  }
+//  file = fopen("./result/hmtafter.txt","w");  
+//  for(int i=0; i<ncsf; i++){
+//    for(int j=0; j<ncsf; j++){
+//	fprintf(file,"%e ",hmt[i][j]);
+//    }	  
+//    fprintf(file,"\n");
+//  }
+
+
+  double complex *cofi = (double complex*)calloc(nfloq, sizeof(double complex));
+#pragma omp parallel for
+  for (int i=0; i<ncsf; i++){
+    cofi[inzr+i] = hmt[i][istate];
+  }  
+  
+
+//  printf("nfloq = %d, tchnl = %d, inzr = %d \n", nfloq, tchnl, inzr);
+
+
+//  file = fopen("./result/iniwav.txt","w");  
+//  for(int i=0; i<nfloq; i++){
+//    fprintf(file,"%e %e\n",creal(cofi[i]), cimag(cofi[i]));
+//  }
+//  fclose(file);  
 
   double complex **cumt1 = calloc_2d_array_complex(ncsf, ncsf);
   double complex **cumt2 = calloc_2d_array_complex(ncsf, ncsf);
-  double complex com1 = -I * dtim;  
-  
-  #pragma omp parallel for collapse(2)
-  for (int i = 0; i < ncsf; i++) {
-    double complex exevl = cexp(com1 * eig[i]);
-    for (int j = 0; j < ncsf; j++) {
-      cumt1[i][j] = exevl * hmt[j][i];
-    }
-  }
 
+  double complex com1 = -I*dtim;  
+  
+   #pragma omp parallel for
+  for(int i=0; i<ncsf; i++){
+    double complex exevl = cexp(com1*eig[i]);
+    for(int j=0; j<ncsf; j++){
+      cumt1[i][j] = exevl*hmt[j][i];
+    }  
+  }
+ 
   free(eig);
 
   copy_matrix_real_to_complex(ncsf, hmt, cumt2); 
-
+  
+//  file = fopen("./result/cumt2.txt","w");  
+//  for(int i=0; i<ncsf; i++){
+//    for(int j=0; j<ncsf; j++){
+//        fprintf(file,"%7.5e ",creal(cumt1[i][j]));
+//    }
+//    fprintf(file,"\n");
+//  }
+//  fclose(file);  
+  
   double complex **exhm = calloc_2d_array_complex(ncsf, ncsf);
 
   double complex *h1, *h2, *h3;
   h1 = (double complex *)calloc(ncsf*ncsf,sizeof(double complex));
   h2 = (double complex *)calloc(ncsf*ncsf,sizeof(double complex));
   h3 = (double complex *)calloc(ncsf*ncsf,sizeof(double complex));
-
-  #pragma omp parallel for collapse(2)
-  for (int i = 0; i < ncsf; i++) {
-    for (int j = 0; j < ncsf; j++) {
-      h1[j*ncsf+i] = cumt1[i][j];        // transposing and storing in h1 and h2
+#pragma omp parallel for collapse(2)
+  for(int i=0; i<ncsf; i++){
+    for(int j=0; j<ncsf; j++){
+    	h1[j*ncsf+i] = cumt1[i][j];        // transposing and storing in h1 and h2
       h2[j*ncsf+i] = cumt2[i][j];        // transpose being done to use cblas_zgemm 
     }	  
   }
@@ -174,47 +265,292 @@ int timeprop(int ncsf, double xmin, double xmax, double lmda, int flqchnl, int n
   double complex alpha = 1.0 + 0.0*I;
   double complex beta = 0.0 + 0.0*I;
 
+
+// lda, ldb, ldc are num_rows_A, num_rows_B and num_rows_C respectively
+
   cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
               ncsf, ncsf, ncsf, &alpha, h2, ncsf, 
               h1, ncsf, &beta, h3, ncsf);
 
-  // Copy result into exhm
-  #pragma omp parallel for collapse(2)
-  for (int i = 0; i < ncsf; i++) {
-    for (int j = 0; j < ncsf; j++) {
+//  file = fopen("./result/exhm.txt","w");  
+//  for(int i=0; i<ncsf; i++){
+//    for(int j=0; j<ncsf; j++){
+//        fprintf(file,"%7.5e ",creal(h3[j*ncsf+i]));
+//    }
+//    fprintf(file,"\n");
+//  }
+//  fclose(file); 
+#pragma omp parallel for collapse(2)
+  for(int i=0; i<ncsf; i++){
+    for(int j=0; j<ncsf; j++){
       exhm[i][j] = h3[j*ncsf+i];
     }
   }
 
-  // Parallelize the time loop
-  #pragma omp parallel for
-  for (int itim = 0; itim < ntim; itim++) {
-    double time = itim * dtim;
-    double epls = epsln * sin((PI * time) / totime) * sin((PI * time) / totime);
-    int n1 = 0;
-    for (int i = 0; i < tchnl; i++) {
-      int findx = -(tchnl + 2)/2 + i;
-      for (int j = 0; j < ncsf; j++) {
-        coff[n1] = cofi[inzr + j] * cexp(findx * I * omga * time);
-        n1++;
-      }
-    }
+//  file = fopen("./result/exhml.txt","w");  
+//  for(int i=0; i<ncsf; i++){
+//    for(int j=0; j<ncsf; j++){
+//        //fprintf(file,"%7.5e ",creal(h3[j*ncsf+i]));
+//        fprintf(file,"%7.5e ",creal(exhm[i][j]));
+//    }
+//    fprintf(file,"\n");
+//  }
+//  fclose(file); 
 
-    // Additional computations can be parallelized similarly
-    // Use similar patterns for other loops like 'eff' calculations, matrix multiplications, etc.
+#pragma omp parallel for collapse(2)
+  for (int i = 0; i < ncsf; i++){
+    for (int j=0; j<ncsf; j++){
+        cumt1[i][j] = CMPLX(0.0,0.0); 
+    }
+  } 
+    #pragma omp parallel for
+
+  for(int i=0; i<ncsf; i++){
+    cumt1[i][i] = cexp(-lmda*cpm[i]*dtim);
+  }
+ 
+  free(cpm);
+
+  copy_matrix_complex_to_complex(ncsf, exhm, cumt2);
+
+    #pragma omp parallel for collapse(2)
+  
+  for(int i=0; i<ncsf; i++){
+    for(int j=0; j<ncsf; j++){
+    	h1[j*ncsf+i] = cumt1[i][j];
+      h2[j*ncsf+i] = cumt2[i][j];
+    }	  
   }
 
-  free(coff);
-  free(cofi);
-  free(cofh);
-  free(h11);
-  free(h22);
-  free(h33);
+  cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+              ncsf, ncsf, ncsf, &alpha, h2, ncsf, 
+              h1, ncsf, &beta, h3, ncsf);
+
+
+
+// ------ This exhm will be used ahead ---------
+#pragma omp parallel for collapse(2)
+for(int i=0; i<ncsf; i++){
+    for(int j=0; j<ncsf; j++){
+      exhm[i][j] = h3[j*ncsf+i];
+    }
+  }
+   
+  
+//  file = fopen("./result/exhml.txt","w");  
+//  for(int i=0; i<ncsf; i++){
+//    for(int j=0; j<ncsf; j++){
+//        fprintf(file,"%7.5e ",creal(exhm[i][j]));
+//    }
+//    fprintf(file,"\n");
+//  }
+//  fclose(file); 
+
+
+  free_2d_array_complex(cumt1, ncsf); free_2d_array_complex(cumt2, ncsf);
+
+
+  for (int i = 0; i < ncsf; i++) {
+        free(hmt[i]);
+  }
+  free(hmt);
+
+
+
+//-------------------------------------------------------------------------
+  cumt1 = calloc_2d_array_complex(tchnl, tchnl);
+  int findx;
+      #pragma omp parallel for
+
+  for(int i=0; i<tchnl; i++){
+    findx = -(tchnl + 1)/2 + (i+1);
+    com1 = -I*omga*0.5*dtim*findx;
+    for(int j = 0; j<tchnl; j++){
+      cumt1[i][j] = sqrt(2.0/(tchnl+1.0))*sin((i+1)*(j+1)*PI/(tchnl+1.0))*cexp(com1);
+    }
+  } 
+
+//  file = fopen("./result/tchnl.txt","w");  
+//  for(int i=0; i<tchnl; i++){
+//    for(int j=0; j<tchnl; j++){
+//        fprintf(file,"%7.5e ",creal(cumt1[i][j]));
+//    }
+//    fprintf(file,"\n");
+//  }
+//  fclose(file); 
+
+
+  
+// ================== Time Propagation starts =======================
+  double complex *coff = (double complex*)calloc(nfloq, sizeof(double complex));
+  double complex *cofh = (double complex*)calloc(ncsf, sizeof(double complex));
+  double complex *cofc = (double complex*)calloc(ncsf, sizeof(double complex));
+  
+  double complex *h11, *h22, *h33;
+  h11 = (double complex *)calloc(ncsf*ncsf, sizeof(double complex));
+  h22 = (double complex *)calloc(ncsf, sizeof(double complex));
+  h33 = (double complex *)calloc(ncsf, sizeof(double complex));
+
+  com1 = -0.5*I*dtim;
+  double time, epls, eff;
+  int n1;
+  double complex csum, dipole;
+
+
+  FILE *timefile = fopen("./result/finalresult.txt","w"); 
+
+
+// ------------- time propagation loop starts ---------------- //
+    #pragma omp parallel for
+
+  for(int itim = 0; itim<ntim; itim++){
+       
+      time = itim*dtim;
+      epls = epsln*sin((PI*time)/totime)*sin((PI*time)/totime);
+              
+      n1 = 0;   
+      for (int i=0; i<tchnl; i++){
+        findx = -(tchnl + 2)/2 + i;
+
+        for (int j=0; j<ncsf; j++){
+            coff[n1] = cofi[inzr+j]*cexp(findx*I*omga*time);
+            n1++;
+        }
+      }
+
+// --------------------------------------------- 
+
+      n1 = 0; eff=0.0;
+          #pragma omp parallel for
+      for (int i = 0; i < nfloq; i++) cofi[i] = CMPLX(0.0,0.0);
+      
+      for (int i=0; i<tchnl; i++){
+        eff = epls*cos((i+1)*PI/(tchnl+1)); 
+        for (int j=0; j<ncsf; j++){
+            csum=CMPLX(0.0,0.0);  
+            for (int k=0; k<tchnl; k++){
+                csum = csum + cumt1[k][i]*coff[ncsf*k+j];
+            }
+            cofi[n1] = csum*cexp(com1*eff*zdipole[j]);
+            n1++;
+        }
+      }
+
+// ------------------ exhm ------------------
+
+      n1 = 0; eff=0;
+ #pragma omp parallel for
+      for (int i = 0; i < nfloq; i++) coff[i] = CMPLX(0.0,0.0);
+#pragma omp parallel for
+      for (int ii=0; ii<tchnl; ii++){
+               
+          int k = ii*ncsf;
+#pragma omp simd
+        
+          for (int j=0; j<ncsf; j++){
+             h22[j] = cofi[k+j]; 
+          }
+        #pragma omp simd
+          
+          for(int i=0; i<ncsf; i++){
+             for(int j=0; j<ncsf; j++){
+            	     h11[j*ncsf+i] = exhm[i][j];
+             }	  
+          }
+        #pragma omp simd
+          
+          for (int i = 0; i < ncsf; i++) h33[i] = CMPLX(0.0,0.0);
+
+          cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+                      ncsf, 1, ncsf, &alpha, h11, ncsf, 
+                      h22, ncsf, &beta, h33, ncsf);
+        #pragma omp simd
+          
+          for (int i=0; i<ncsf; i++){
+             coff[k+i] = h33[i];
+          } 
+
+      }
+
+// -------------------------------------
+
+      n1 = 0; eff=0;
+    #pragma omp parallel for
+
+  	for (int i = 0; i < nfloq; i++) cofi[i] = CMPLX(0.0,0.0);
+      
+	for (int i=0; i<tchnl; i++){
+          eff = epls*cos((i+1)*PI/(tchnl+1));
+          for (int j=0; j<ncsf; j++){
+              cofi[n1] = cexp(com1*zdipole[j]*eff)*coff[n1];
+              n1++;
+          }
+      }
+       
+      n1 = 0;
+       #pragma omp parallel for
+      for (int i = 0; i < nfloq; i++) coff[i] = CMPLX(0.0,0.0);
+ #pragma omp parallel for collapse(2)
+      for (int j=0; j<tchnl; j++){
+          for (int i=0; i<ncsf; i++){
+              csum=CMPLX(0.0,0.0);  
+              for (int k=0; k<tchnl; k++){
+                 csum = csum + cumt1[j][k]*cofi[ncsf*k+i];
+              }
+              coff[n1] = csum;
+              n1++;
+          }
+      }
+
+//      file = fopen("./result/cofh.txt","w");  
+//      for(int i=0; i<nfloq; i++){
+//          fprintf(file,"%7.5e %7.5e \n",creal(coff[i]),cimag(coff[i]));
+//      }
+//      fclose(file); 
+
+#pragma omp parallel for
+      for (int i = 0; i < nfloq; i++) cofi[i] = CMPLX(0.0,0.0);
+#pragma omp parallel for
+      for (int i=0; i<nfloq; i++){
+        cofi[i] = coff[i];
+      }
+      #pragma omp parallel for
+      for (int i = 0; i < ncsf; i++) cofh[i] = CMPLX(0.0,0.0);
+#pragma omp parallel for
+      for (int i=0; i<ncsf; i++){
+        cofh[i] = coff[inzr+i];
+      }
+    
+//      file = fopen("./result/cofh.txt","w");  
+//      for(int i=0; i<ncsf; i++){
+//          fprintf(file,"%7.5e %7.5e \n",creal(cofh[i]),cimag(cofh[i]));
+//      }
+//      fclose(file); 
+
+      dipole = CMPLX(0.0,0.0);
+      #pragma omp parallel for
+      for (int i=0; i<ncsf; i++){
+        dipole += conj(cofh[i])*epls*zdipole[i]*cos(omga*time)*cofh[i];
+      }
+
+      fprintf(timefile,"%e %e %e %e \n",time/tau, epls*cos(omga*time), creal(dipole), cimag(dipole)); 
+
+
+  }   // time loop ends
+  fclose(timefile);  
+
+
+// free memory
+  free(coff); free(cofi); free(cofh);
+  free(h11); free(h22); free(h33);
   free(zdipole);
-  free_2d_array_complex(cumt1, tchnl);
+
+  free_2d_array_complex(cumt1, tchnl); 
 
   return 0;
 }
+
+
 
 int main(){
   int ntime, ncstep, istate, flqchnl;
@@ -222,20 +558,23 @@ int main(){
   double xmin, xmax, epsln, omga;
   double tau, totime, lmda;
 
-  ncsf = 1000; flqchnl = 5; xmin = 0 ; xmax = 10.0;
+  ncsf = 100; flqchnl = 5; xmin = 0 ; xmax = 10.0;
   epsln = 0.5; omga = 0.5; lmda = 0.0;
-  noptc = 5; istate = 1;
+  noptc = 5; istate = 1;           //keep istate as 1, 0 giving wrong result
   
   tau = 2.0*PI/omga;
   totime = noptc*tau;
   ntime = (int)totime;
   
   printf("ntime = %d, totime = %lf, tau = %lf\n",ntime, totime, tau);
-float start=omp_get_wtime();
+double start_time = omp_get_wtime();
   timeprop(ncsf, xmin, xmax, lmda, flqchnl, noptc, istate, totime, ntime, omga, epsln);
-  float stop=omp_get_wtime();
-  printf("time taken%f",stop-start);
+    double end_time = omp_get_wtime();
+
+  double total_time = end_time - start_time;
+
+    // Print the elapsed time
+    printf("Total time: %f seconds\n", total_time);
 
   return 0;
 }
-
